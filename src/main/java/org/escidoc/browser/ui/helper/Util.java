8febcc1a82eba6ea2d9d7f6775d28658656dad67
crh <christian.herlambang@fiz-karlsruhe.de>
2011-06-30 16:10:57 +0000

    fixed top level members query

--- src/main/java/org/escidoc/browser/ui/helper/Util.java
+++ src/main/java/org/escidoc/browser/ui/helper/Util.java
@@ -151,4 +151,22 @@ public final class Util {
         stringBuilder.append(id);
         return stringBuilder.toString();
     }
+
+    public static SearchRetrieveRequestType createQueryForTopLevelContainersAndItems(final String id) {
+        Preconditions.checkNotNull(id, "id is null: %s", id);
+        Preconditions.checkArgument(!id.isEmpty(), "id is empty: %s", id);
+        final SearchRetrieveRequestType filter = new SearchRetrieveRequestType();
+        filter.setQuery(topLevelContainersAndItems(id));
+        return filter;
+    }
+
+    private static String topLevelContainersAndItems(final String id) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        final String queryString =
+            "(top-level-containers=true or top-level-items=true) and \"/properties/context/id\"=";
+        stringBuilder.append(queryString);
+        stringBuilder.append(id);
+        final String query = stringBuilder.toString();
+        return query;
+    }
 }
