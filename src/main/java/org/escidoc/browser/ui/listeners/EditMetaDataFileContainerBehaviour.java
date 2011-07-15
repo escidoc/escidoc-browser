ea1088cfa088d48471ffde1dc26209d4519b8a91
abrahaj <armand.brahaj@fiz-karlsruhe.de>
2011-07-15 13:18:05 +0000

    Minor change

---
+++ src/main/java/org/escidoc/browser/ui/listeners/EditMetaDataFileContainerBehaviour.java
@@ -0,0 +1,24 @@
+package org.escidoc.browser.ui.listeners;
+
+import org.escidoc.browser.model.EscidocServiceLocation;
+
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Window;
+
+import de.escidoc.core.resources.common.MetadataRecord;
+
+public class EditMetaDataFileContainerBehaviour implements ClickListener {
+
+    public EditMetaDataFileContainerBehaviour(MetadataRecord metadataRecord, Window mainWindow,
+        EscidocServiceLocation escidocServiceLocation) {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void buttonClick(ClickEvent event) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
