73af8783706fd9499d39298c2938483faf7c2129
Christian Herlambang <christian.herlambang@gmail.com>
2011-10-21 13:34:11 +0200

    refactoring: reordering fields, removed comments and unused fields. Add TODO for discussion and fix

--- src/main/java/org/escidoc/browser/elabsmodul/views/StudyView.java
+++ src/main/java/org/escidoc/browser/elabsmodul/views/StudyView.java
@@ -1,8 +1,16 @@
 package org.escidoc.browser.elabsmodul.views;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import com.google.common.base.Preconditions;
+
+import com.vaadin.data.util.POJOItem;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.VerticalLayout;
 
 import org.escidoc.browser.elabsmodul.constants.ELabsViewContants;
 import org.escidoc.browser.elabsmodul.interfaces.ILabsAction;
@@ -20,71 +28,62 @@ import org.escidoc.browser.ui.view.helpers.DirectMember;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Preconditions;
-import com.vaadin.data.util.POJOItem;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickListener;
-import com.vaadin.ui.Component;
-import com.vaadin.ui.CssLayout;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.Panel;
-import com.vaadin.ui.VerticalLayout;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import de.escidoc.core.client.exceptions.EscidocClientException;
 
 public class StudyView extends Panel implements ILabsPanel, ILabsAction {
 
-    private static final String RIGHT_PANEL = null;
+    private static Logger LOG = LoggerFactory.getLogger(StudyView.class);
 
-    private POJOItem<StudyBean> pojoItem = null;
+    private static final String RIGHT_PANEL = "";
 
-    private StudyBean studyBean = null, lastStateBean = null;
+    private static final String[] PROPERTIES = ELabsViewContants.STUDY_PROPERTIES;
 
-    private ISaveAction saveComponent;
+    private final StudyBean studyBean;
 
-    private VerticalLayout mainLayout = null, dynamicLayout = null;
+    private final ISaveAction saveComponent;
 
-    private List<ResourceModel> breadCrumbModel;
+    private final List<ResourceModel> breadCrumbModel;
 
-    private ContainerProxy containerProxy;
-
-    private final String[] PROPERTIES = ELabsViewContants.STUDY_PROPERTIES;
-
-    final String VIEWCAPTION = "Instument View";
-
-    final String LAST_MODIFIED_BY = "Last modification by ";
-
-    final String FLOAT_LEFT = "floatleft";
-
-    final String FLOAT_RIGHT = "floatright";
+    private final ContainerProxy containerProxy;
 
     private final int COMPONENT_COUNT = 4;
 
-    private HorizontalLayout buttonLayout = null;
+    private final CssLayout cssLayout = new CssLayout();
 
-    private List<HorizontalLayout> registeredComponents = null;
+    private final Router router;
+
+    private POJOItem<StudyBean> pojoItem;
+
+    private VerticalLayout mainLayout, dynamicLayout;
+
+    private HorizontalLayout buttonLayout;
+
+    private List<HorizontalLayout> registeredComponents;
 
     private LabsClientViewEventHandler clientViewEventHandler;
 
-    private ClickListener mouseClickListener = null;
+    private ClickListener mouseClickListener;
 
-    private static Logger LOG = LoggerFactory.getLogger(StudyView.class);
+    private HorizontalLayout modifiedComponent;
 
-    private HorizontalLayout modifiedComponent = null;
+    public StudyView(final StudyBean sourceBean, final ISaveAction saveComponent,
+        final List<ResourceModel> breadCrumbModel, final ResourceProxy resourceProxy, final Router router) {
+        Preconditions.checkNotNull(sourceBean, "sourceBean is null: %s", sourceBean);
+        Preconditions.checkNotNull(saveComponent, "saveComponent is null: %s", saveComponent);
+        Preconditions.checkNotNull(breadCrumbModel, "breadCrumbModel is null: %s", breadCrumbModel);
+        Preconditions.checkNotNull(resourceProxy, "resourceProxy is null: %s", resourceProxy);
+        Preconditions.checkNotNull(router, "router is null: %s", router);
 
-    private final CssLayout cssLayout = new CssLayout();
-
-    private Router router;
-
-    public StudyView(StudyBean sourceBean, ISaveAction saveComponent, List<ResourceModel> breadCrumbModel,
-        ResourceProxy resourceProxy, Router router) {
         this.studyBean = (sourceBean != null) ? sourceBean : new StudyBean();
-        this.lastStateBean = studyBean;
         this.saveComponent = saveComponent;
         this.breadCrumbModel = breadCrumbModel;
         this.containerProxy = (ContainerProxy) resourceProxy;
         this.router = router;
+
         initialisePanelComponents();
         buildContainerGUI();
         buildPropertiesGUI();
@@ -93,17 +92,18 @@ public class StudyView extends Panel implements ILabsPanel, ILabsAction {
         createClickListener();
     }
 
+    @SuppressWarnings("serial")
     private void createClickListener() {
         mouseClickListener = new Button.ClickListener() {
-            private static final long serialVersionUID = -8330004043242560612L;
 
             @Override
-            public void buttonClick(com.vaadin.ui.Button.ClickEvent event) {
+            public void buttonClick(final com.vaadin.ui.Button.ClickEvent event) {
                 if (event.getButton().getCaption().equals("Save")) {
                     saveComponent.saveAction(studyBean);
+
+                    // TODO Why do we need to call these two methods?
                     StudyView.this.resetLayout();
                     dynamicLayout.requestRepaintAll();
-
                 }
             }
         };
@@ -111,7 +111,8 @@ public class StudyView extends Panel implements ILabsPanel, ILabsAction {
         try {
             ((Button) buttonLayout.getComponent(1)).addListener(mouseClickListener);
         }
-        catch (ClassCastException e) {
+        catch (final ClassCastException e) {
+            // TODO report error to user?
             LOG.error(e.getMessage());
         }
 
@@ -121,8 +122,8 @@ public class StudyView extends Panel implements ILabsPanel, ILabsAction {
         Preconditions.checkNotNull(dynamicLayout, "View's dynamiclayout is null.");
 
         HorizontalLayout tempParentLayout = null;
-        for (Iterator<Component> iterator = dynamicLayout.getComponentIterator(); iterator.hasNext();) {
-            Component component = iterator.next();
+        for (final Iterator<Component> iterator = dynamicLayout.getComponentIterator(); iterator.hasNext();) {
+            final Component component = iterator.next();
             if (component instanceof HorizontalLayout) {
                 tempParentLayout = (HorizontalLayout) component;
             }
@@ -159,16 +160,16 @@ public class StudyView extends Panel implements ILabsPanel, ILabsAction {
         dynamicLayout.setStyleName(ELabsViewContants.STYLE_ELABS_FORM);
 
         this.buttonLayout = LabsLayoutHelper.createButtonLayout();
-        HorizontalLayout h1 =
+        final HorizontalLayout h1 =
             LabsLayoutHelper.createHorizontalLayoutWithELabsLabelAndLabelData(ELabsViewContants.L_STUDY_TITLE,
                 pojoItem.getItemProperty(ELabsViewContants.P_STUDY_TITLE));
-        HorizontalLayout h2 =
+        final HorizontalLayout h2 =
             LabsLayoutHelper.createHorizontalLayoutWithELabsLabelAndLabelData(ELabsViewContants.L_STUDY_DESC,
                 getPojoItem().getItemProperty(ELabsViewContants.P_STUDY_DESC));
-        HorizontalLayout h3 =
+        final HorizontalLayout h3 =
             LabsLayoutHelper.createHorizontalLayoutWithELabsLabelAndLabelData(ELabsViewContants.L_STUDY_MOT_PUB,
                 getPojoItem().getItemProperty(ELabsViewContants.P_STUDY_MOT_PUB));
-        HorizontalLayout h4 =
+        final HorizontalLayout h4 =
             LabsLayoutHelper.createHorizontalLayoutWithELabsLabelAndLabelData(ELabsViewContants.L_STUDY_RES_PUB,
                 getPojoItem().getItemProperty(ELabsViewContants.P_STUDY_RES_PUB));
 
@@ -184,7 +185,6 @@ public class StudyView extends Panel implements ILabsPanel, ILabsAction {
 
         dynamicLayout.addComponent(new HorizontalLayout(), 4);
 
-        // this.mainLayout.addComponent(this.dynamicLayout);
         rightCell(dynamicLayout);
         mainLayout.addComponent(cssLayout);
         mainLayout.setExpandRatio(cssLayout, 1.0f);
@@ -208,8 +208,8 @@ public class StudyView extends Panel implements ILabsPanel, ILabsAction {
         try {
             leftCell();
         }
-        catch (EscidocClientException e) {
-            // TODO Auto-generated catch block
+        catch (final EscidocClientException e) {
+            // TODO log exception and tell the user something going wrong
             e.printStackTrace();
         }
     }
@@ -220,6 +220,7 @@ public class StudyView extends Panel implements ILabsPanel, ILabsAction {
      * 
      * @param comptoBind
      */
+    // TODO why deprecated?
     @SuppressWarnings("deprecation")
     private void rightCell(final Component comptoBind) {
         final Panel rightpnl = new Panel();
@@ -236,6 +237,7 @@ public class StudyView extends Panel implements ILabsPanel, ILabsAction {
         cssLayout.addComponent(rightpnl);
     }
 
+    // TODO why deprecated?
     @SuppressWarnings("deprecation")
     private void leftCell() throws EscidocClientException {
         final Panel leftPanel = new Panel();
@@ -265,7 +267,8 @@ public class StudyView extends Panel implements ILabsPanel, ILabsAction {
             try {
                 horizontalLayout = (HorizontalLayout) dynamicLayout.getComponent(COMPONENT_COUNT);
             }
-            catch (ClassCastException e) {
+            catch (final ClassCastException e) {
+                // TODO log exception and tell the user something going wrong
                 LOG.error(e.getMessage());
             }
             if (horizontalLayout != null) {
@@ -281,11 +284,12 @@ public class StudyView extends Panel implements ILabsPanel, ILabsAction {
     }
 
     @Override
-    public void setModifiedComponent(Component modifiedComponent) {
+    public void setModifiedComponent(final Component modifiedComponent) {
         try {
             this.modifiedComponent = (HorizontalLayout) modifiedComponent;
         }
-        catch (ClassCastException e) {
+        catch (final ClassCastException e) {
+            // TODO tell the user something going wrong
             LOG.error(e.getMessage());
         }
     }
@@ -301,7 +305,8 @@ public class StudyView extends Panel implements ILabsPanel, ILabsAction {
             try {
                 ((HorizontalLayout) dynamicLayout.getComponent(COMPONENT_COUNT)).removeAllComponents();
             }
-            catch (ClassCastException e) {
+            catch (final ClassCastException e) {
+                // TODO tell the user something going wrong
                 LOG.error(e.getMessage());
             }
         }
