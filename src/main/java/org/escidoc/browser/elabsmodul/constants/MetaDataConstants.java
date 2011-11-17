db75349211e2d0a8e70b3e60093ddfbdca711c80
abrahaj <armand.brahaj@fiz-karlsruhe.de>
2011-11-17 16:38:36 +0100

    Fixed: GENCLIENT-229: Instrument's name is reflecting the desired one added in the web form.

--- src/main/java/org/escidoc/browser/elabsmodul/constants/MetaDataConstants.java
+++ src/main/java/org/escidoc/browser/elabsmodul/constants/MetaDataConstants.java
@@ -2,18 +2,17 @@ package org.escidoc.browser.elabsmodul.constants;
 
 public class MetaDataConstants {
     public static final String INSTRUMENT =
-        "<el:Instrument xmlns:el=\"http://escidoc.org/ontologies/bw-elabs/re#\">"
-            + "<dc:title xmlns:dc=\"http://purl.org/dc/elements/1.1/\">Default Instrument 01</dc:title>"
-            + "<dc:description xmlns:dc=\"http://purl.org/dc/elements/1.1/\">An example instrument for eLabs development.</dc:description>"
-            + "<el:identity-number></el:identity-number>"
+        "<el:Instrument xmlns:el=\"http://escidoc.org/ontologies/bw-elabs/re#\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\">"
+            + "<dc:title>FRS Instrument 01</dc:title>"
+            + "<dc:description>An example instrument for eLabs development.</dc:description>"
+            + "<el:identity-number>0001</el:identity-number>"
             + "<el:requires-configuration>no</el:requires-configuration>"
             + "<el:requires-calibration>no</el:requires-calibration>"
             + "<el:esync-endpoint>http://my.es/ync/endpoint</el:esync-endpoint>"
             + "<el:monitored-folder>C:\\tmp\\monitored0001</el:monitored-folder>"
-            + "<el:result-mime-type>FMF</el:result-mime-type>"
-            + "<el:responsible-person xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" rdf:resource=\"escidoc:42\"></el:responsible-person>"
-            + "<el:institution xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" rdf:resource=\"escidoc:1001\"></el:institution>"
-            + "</el:Instrument>";
+            + "<el:result-mime-type>application/octet-stream</el:result-mime-type>"
+            + "<el:responsible-person rdf:resource=\"escidoc:42\"/>"
+            + "<el:institution rdf:resource=\"escidoc:1001\"/>" + "</el:Instrument>";
 
     public static final String STUDY =
         "<el:Study xmlns:el=\"http://escidoc.org/ontologies/bw-elabs/re#\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\">"
