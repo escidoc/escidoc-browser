37e4df98a7a6348a4cb8cdd3a4a795b88525cd3c
abrahaj <armand.brahaj@fiz-karlsruhe.de>
2012-02-08 13:31:42 +0100

    Added Add resource plus sign on header of OU View

--- src/main/java/org/escidoc/browser/ui/maincontent/OrgUnitMetadataRecordsView.java
+++ src/main/java/org/escidoc/browser/ui/maincontent/OrgUnitMetadataRecordsView.java
@@ -1,17 +1,6 @@
 package org.escidoc.browser.ui.maincontent;
 
-import com.google.common.base.Preconditions;
-
-import com.vaadin.terminal.ExternalResource;
-import com.vaadin.ui.Accordion;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Component;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.Link;
-import com.vaadin.ui.Panel;
-import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.themes.BaseTheme;
+import java.net.URISyntaxException;
 
 import org.escidoc.browser.model.ResourceProxy;
 import org.escidoc.browser.repository.internal.ActionIdConstants;
@@ -21,7 +10,20 @@ import org.escidoc.browser.ui.ViewConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.URISyntaxException;
+import com.google.common.base.Preconditions;
+import com.vaadin.terminal.ExternalResource;
+import com.vaadin.terminal.ThemeResource;
+import com.vaadin.ui.Accordion;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Link;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.BaseTheme;
+import com.vaadin.ui.themes.Runo;
 
 import de.escidoc.core.client.exceptions.EscidocClientException;
 import de.escidoc.core.resources.common.MetadataRecord;
@@ -58,23 +60,47 @@ public class OrgUnitMetadataRecordsView {
         return accordion;
     }
 
+    public Panel asPanel() {
+        final Panel pnlmetadataRecs = new Panel();
+        pnlmetadataRecs.setSizeFull();
+        pnlmetadataRecs.setStyleName(Runo.PANEL_LIGHT);
+        VerticalLayout vl = new VerticalLayout();
+        vl.setImmediate(false);
+        vl.setWidth("100.0%");
+        vl.setHeight("100.0%");
+        vl.setMargin(false);
+        vl.addComponent(buildMetaDataTab());
+
+        pnlmetadataRecs.setContent(vl);
+        return pnlmetadataRecs;
+    }
+
     private Component buildMetaDataTab() {
         Panel innerPanel = new Panel();
-        innerPanel.setHeight("100%");
+        innerPanel.setSizeFull();
+        VerticalLayout vl = new VerticalLayout();
+        vl.setSizeFull();
+
+        final CssLayout cssLayout = new CssLayout();
+        cssLayout.setHeight("20px");
+        buildPanelHeader(cssLayout, ViewConstants.METADATA);
+        ThemeResource ICON = new ThemeResource("images/assets/plus.png");
 
         if (canAddMetadata()) {
-            final Button addNewOrgUnitBtn =
-                new Button(ViewConstants.ADD_NEW_META_DATA, new OnAddOrgUnitMetadata(ou, router.getRepositories(),
-                    router.getMainWindow()));
+            final Button addNewOrgUnitBtn = new Button();
+            addNewOrgUnitBtn
+                .addListener(new OnAddOrgUnitMetadata(ou, router.getRepositories(), router.getMainWindow()));
             addNewOrgUnitBtn.setStyleName(BaseTheme.BUTTON_LINK);
-            innerPanel.addComponent(addNewOrgUnitBtn);
+            addNewOrgUnitBtn.addStyleName("floatright paddingtop3");
+            addNewOrgUnitBtn.setWidth("20px");
+            addNewOrgUnitBtn.setIcon(ICON);
+            cssLayout.addComponent(addNewOrgUnitBtn);
         }
-
+        vl.addComponent(cssLayout);
         final MetadataRecords mdList = ou.getMedataRecords();
         for (final MetadataRecord metadataRecord : mdList) {
             final HorizontalLayout hl = new HorizontalLayout();
             hl.setStyleName("metadata");
-            vl.addComponent(hl);
 
             Link mdLink =
                 new Link(metadataRecord.getName(), new ExternalResource(router.getServiceLocation().getEscidocUri()
@@ -84,7 +110,6 @@ public class OrgUnitMetadataRecordsView {
             mdLink.setDescription("Show metadata information in a separate window");
             hl.addComponent(mdLink);
             hl.addComponent(new Label("&nbsp; | &nbsp;", Label.CONTENT_RAW));
-
             if (canAddMetadata()) {
                 final Button editMdBtn =
                     new Button("edit", new OnEditOrgUnitMetadata(metadataRecord, router, router.getRepositories(), ou,
@@ -93,11 +118,23 @@ public class OrgUnitMetadataRecordsView {
                 editMdBtn.setDescription("Replace the metadata with a new content file");
                 hl.addComponent(editMdBtn);
             }
-            innerPanel.addComponent(hl);
+            vl.addComponent(hl);
+            vl.setExpandRatio(hl, 9);
         }
-        outerPanel.addComponent(new Label("&nbsp;", Label.CONTENT_RAW));
-        outerPanel.addComponent(vl);
+
+        innerPanel.setContent(vl);
         return innerPanel;
+    }
+
+    private void buildPanelHeader(CssLayout cssLayout, String name) {
+        cssLayout.addStyleName("v-accordion-item-caption v-caption v-captiontext");
+        cssLayout.setWidth("100%");
+        cssLayout.setMargin(false);
+
+        final Label nameofPanel = new Label(name, Label.CONTENT_RAW);
+        nameofPanel.setStyleName("accordion v-captiontext");
+        nameofPanel.setWidth("70%");
+        cssLayout.addComponent(nameofPanel);
     }
 
     private boolean canAddMetadata() {
