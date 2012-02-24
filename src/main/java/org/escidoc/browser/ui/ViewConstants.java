0a532d8d493da496b17bba8166a3286f839eb5e6
cherlam <christian.herlambang@fiz-karlsruhe.de>
2012-02-24 14:13:29 +0100

    Preparing for 0.4.1 Release
    + Update project Version number in pom.xml and View

--- src/main/java/org/escidoc/browser/ui/ViewConstants.java
+++ src/main/java/org/escidoc/browser/ui/ViewConstants.java
@@ -31,7 +31,7 @@ package org.escidoc.browser.ui;
 public class ViewConstants {
 
     // Footer
-    public static final String VERSION = "0.4.1-SNAPSHOT";
+    public static final String VERSION = "0.4.1";
 
     public static final String PRODUCT_NAME = " eSciDoc Browser ";
 
