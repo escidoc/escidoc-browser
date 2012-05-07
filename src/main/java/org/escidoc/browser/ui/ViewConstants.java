5ff5f9ba9e9450b2a268be52cb2f216877caa358
Arm Brahaj <armand.brahaj@fiz-karlsruhe.de>
2012-05-07 14:53:32 +0200

    Changing to release version 0.4.5

--- src/main/java/org/escidoc/browser/ui/ViewConstants.java
+++ src/main/java/org/escidoc/browser/ui/ViewConstants.java
@@ -31,7 +31,7 @@ package org.escidoc.browser.ui;
 public class ViewConstants {
 
     // Footer
-    public static final String VERSION = "0.4.4";
+    public static final String VERSION = "0.4.5";
 
     public static final String PRODUCT_NAME = " eSciDoc Browser ";
 
